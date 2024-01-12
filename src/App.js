6eb6aa4267aedd634c71be28b4e4f9ca675ffa2e
neeoll <arias.noel24@gmail.com>
2024-01-12 10:04:29 -0500

    Typewriter effect and z-index fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { SiLinkedin, SiGithub, SiGmail } from "react-icons/si"
+import * as TypewriterEffect from "typewriter-effect"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <ul className="contact-links">
-          <li className='contact-link'><a href="http://linkedin.com/in/noel-arias" target="_blank"><SiLinkedin /></a></li>
-          <li className='contact-link'><a href="http://github.com/neeoll" target="_blank"><SiGithub /></a></li>
-          <li className='contact-link'><a href="mailto: arias.noel24@gmail.com" target="_blank"><SiGmail /></a></li>
+          <li className='contact-link'><a href="http://linkedin.com/in/noel-arias" target="_blank" rel="noreferrer"><SiLinkedin /></a></li>
+          <li className='contact-link'><a href="http://github.com/neeoll" target="_blank" rel="noreferrer"><SiGithub /></a></li>
+          <li className='contact-link'><a href="mailto: arias.noel24@gmail.com" target="_blank" rel="noreferrer"><SiGmail /></a></li>
         </ul>
-        <div className="body" id='body'>
+        <div className="body">
           <div className='text-block'>
-            <pre>{`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget malesuada libero, a elementum enim. Mauris aliquet est a mi sollicitudin, vel volutpat dui commodo.\n\nDonec diam dolor, sodales eget lacinia sed, convallis ut diam. Donec commodo laoreet sapien, at hendrerit leo ullamcorper eget. Maecenas porttitor mattis convallis. Phasellus posuere vestibulum ornare. Sed finibus tellus non eros dictum laoreet. Suspendisse dapibus quis ligula sit amet euismod. Integer sodales volutpat condimentum.\n\nDonec sit amet nisi quam.`}</pre>
+            <pre>
+              <TypewriterEffect.default onInit={(typewriter) => {
+                typewriter.changeDelay(35)
+                .typeString("Hello World!")
+                .pauseFor(Math.random() * (750 - 500) + 500)
+                .typeString("\n\nMy name is Noel Arias, I'm a self taught software engineer from North Carolina with a few years of experience working across multiple software development disciplines.")
+                .pauseFor(Math.random() * (750 - 500) + 500)
+                .typeString("\nI've built a number of personal projects and contribute to open source software on my spare time.")
+                .pauseFor(Math.random() * (750 - 500) + 500)
+                .typeString(" I’m a quick learner and am looking for a position where I can expand my skill set and learn from other developers.")
+                .pauseFor(Math.random() * (750 - 500) + 500)
+                .typeString("\nOn the right you can find a short list of projects I've worked on, and up top are my LinkedIn, Github, and email address.")
+                .pauseFor(Math.random() * (750 - 500) + 500)
+                .typeString(" I look forward to working with you!")
+                .start()
+              }} />
+            </pre>
           </div>
           <div className='projects'>
+            <pre className="projects-header">Projects</pre>
             <ul className='project-links'>
-              <li className='project-link'><pre><a href="https://guardianbot.space/" target="_blank">Guardian Bot</a></pre></li>
-              <li className='project-link'><pre><a href="https://social-media-roan.vercel.app/" target="_blank">Discord Clone</a></pre></li>
-              <li className='project-link'><pre><a href="https://web3-todo-list-6f1zsfbty-ariasnoel.vercel.app/" target="_blank">Tudu</a></pre></li>
+              <li className='project-link'><pre><a href="https://guardianbot.space/" target="_blank" rel="noreferrer">Guardian</a></pre></li>
+              <li className='project-link'><pre><a href="https://social-media-roan.vercel.app/" target="_blank" rel="noreferrer">Chat App</a></pre></li>
+              <li className='project-link'><pre><a href="https://web3-todo-list-6f1zsfbty-ariasnoel.vercel.app/" target="_blank" rel="noreferrer">Tudu</a></pre></li>
             </ul>
           </div>
         </div>
